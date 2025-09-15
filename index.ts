eafb3e714d03aa9c924c5a879fc1fb2dcf5a6f32
com-trustscore <trustscore.com@gmail.com>
2025-09-15 12:02:07 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-export const fetch=z1=>z2=>z3=>{
-  const html = `<!doctype html><html><head><meta charset="utf-8"><title>日本語テスト</title></head><body><h1>こんにちは、世界！</h1><p>ここに日本語のテキストがあります。</p></body></html>`;
+export const fetch=z1=>z2=>async z3=>{
+  // const html = `<!doctype html><html><head><meta charset="utf-8"><title>日本語テスト</title></head><body><h1>こんにちは、世界！</h1><p>ここに日本語のテキストがあります。</p></body></html>`;
 
-  return new Response(html, {
-    headers: {
-      [`Content-Type`]: `text/html; charset=utf-8`
-    }
-  });
+  // return new Response(html, {
+  //   headers: {
+  //     [`Content-Type`]: `text/html; charset=utf-8`
+  //   }
+  // });
+
+  return await z2.ASSETS.fetch(z1);
 };
 export default {
   // eslint-disable-next-line @typescript-eslint/max-params
-  fetch: (z1, z2, z3)=>fetch (z1) (z2) (z3),
+  fetch: async (z1, z2, z3)=>await fetch (z1) (z2) (z3),
 };
