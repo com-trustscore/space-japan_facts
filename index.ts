63d61b3ca4ba61853d8553d16ed24f106fbcfd93
com-trustscore <trustscore.com@gmail.com>
2025-09-15 11:29:45 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 export const fetch=z1=>z2=>z3=>{
-  console.log(`abc`);
-  return z2.ASSETS.fetch (z1);
+  const html = `<!doctype html><html><head><meta charset="utf-8"><title>日本語テスト</title></head><body><h1>こんにちは、世界！</h1><p>ここに日本語のテキストがあります。</p></body></html>`;
+
+  return new Response(html, {
+    headers: {
+      [`Content-Type`]: `text/html; charset=utf-8`
+    }
+  });
 };
 export default {
   // eslint-disable-next-line @typescript-eslint/max-params
